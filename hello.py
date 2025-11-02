4d6695ac2320d19317e36e421f8612e6e44aadd0
umar rabiu baba <umarrabiu7182@gmail.com>
2025-11-02 05:17:24 +0000

    list add to hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print('hello git from python')
+num = [2,4,6,8]
+for i in range (len(num)):
+	print(num)
+
+numbers = [2, 4, 6, 8]
+doubled = [] 
+for n in numbers:
+    doubled.append(n * 2) 
+print("Original list:", numbers)
+print("Doubled list:", doubled)
