edeb94a9c1ccc15fe45288ba7db0dcc47427b07c
umar rabiu baba <umarrabiu7182@gmail.com>
2025-11-03 05:20:06 +0000

    testing -amend command

--- hello.py
+++ hello.py
@@ -7,5 +7,7 @@ numbers = [2, 4, 6, 8]
 doubled = [] 
 for n in numbers:
     doubled.append(n * 2) 
-print("Original list:", numbers)
+print("Original list:", numbers) 
 print("Doubled list:", doubled)
+print("Login feature")
+print("Payment feature")
